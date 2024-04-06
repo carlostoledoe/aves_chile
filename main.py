36dbc0a006020daec15d9184d4d4d36312aa61a4
Carlos Toledo <109118034+carlostoledoe@users.noreply.github.com>
2024-04-06 11:57:06 -0300

    Se agrega comentario

--- main.py
+++ main.py
@@ -3,4 +3,5 @@ from module import request_get, html_card, crear_html
 # Url a consultar
 url =  'https://aves.ninjas.cl/api/birds'
 
+# Se ejecuta tres funciones de forma anidada
 crear_html(html_card(request_get(url)))
